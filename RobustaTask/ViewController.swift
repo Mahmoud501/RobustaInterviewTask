901255bd36bedca1d1afd971f17db77dae1cc02d
Ahmed <ahmedkamel89@gmail.com>
2020-12-08 11:59:19 +0200

    Initial Commit

---
+++ RobustaTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RobustaTask
+//
+//  Created by MAJED A  ALGARNI on 12/8/20.
+//  Copyright © 2020 Wakeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RobustaTaskTests/RobustaTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  RobustaTaskTests.swift
+//  RobustaTaskTests
+//
+//  Created by MAJED A  ALGARNI on 12/8/20.
+//  Copyright © 2020 Wakeb. All rights reserved.
+//
+
+import XCTest
+@testable import RobustaTask
+
+class RobustaTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
